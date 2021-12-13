91643b6d4700b6667d367d229ca200c7d82b56f2
igor <ighor.naumov.2002mail@mail.ru>
2021-12-14 01:44:22 +0500

    add game_object.py

---
+++ modules/game_object.py
@@ -0,0 +1,24 @@
+from abc import ABC
+
+from pygame.math import Vector2
+
+from modules.utils import wrap_position
+
+
+class GameObject(ABC):
+    def __init__(self, position, sprite, velocity):
+        self.position = Vector2(position)
+        self.sprite = sprite
+        self.radius = sprite.get_width() / 2
+        self.velocity = Vector2(velocity)
+
+    def draw(self, surface):
+        blit_position = self.position - Vector2(self.radius)
+        surface.blit(self.sprite, blit_position)
+
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
+
+    def collides_with(self, other_obj):
+        distance = self.position.distance_to(other_obj.position)
+        return distance < self.radius + other_obj.radius

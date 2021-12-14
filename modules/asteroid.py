83f6048e53fe124641925eea262a647dfed266a8
igor <ighor.naumov.2002mail@mail.ru>
2021-12-14 19:59:52 +0500

    applying modul.color WHITE

--- modules/asteroid.py
+++ modules/asteroid.py
@@ -5,10 +5,11 @@ from pygame.math import Vector2
 from modules.game_object import GameObject
 from modules.utils import get_random_velocity
 from modules.asteroids_surfaces import drawing_asteroid_in_surface, creating_first_type_location_points_to_asteroid
+from modules.colors import WHITE
 
 
 class Asteroid(GameObject):
     def __init__(self, position: Union[Tuple[int, int], Vector2], width: int, height: int):
-        asteroid = drawing_asteroid_in_surface(width, height, creating_first_type_location_points_to_asteroid)
+        asteroid = drawing_asteroid_in_surface(width, height, WHITE, creating_first_type_location_points_to_asteroid)
         super().__init__(position, asteroid, get_random_velocity(2, 4))
 

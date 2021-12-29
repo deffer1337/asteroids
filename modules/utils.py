023d56b0a4270518132e097c4ff332cc15af5428
igor <ighor.naumov.2002mail@mail.ru>
2021-12-29 16:53:00 +0500

    add tail to spaceship, random position to object, collides two object

--- modules/utils.py
+++ modules/utils.py
@@ -45,3 +45,38 @@ def create_spaceship_picture(spaceship_width: int, spaceship_height: int, color:
     draw.aalines(spaceship, color, False, points, 3)
 
     return spaceship
+
+
+def draw_tail_in_spaceship(spaceship: Surface, color: Tuple[int, int, int]) -> Surface:
+    """
+    :param spaceship: Surface spaceship
+    :param color: Spaceship_tail color
+    :return: Surface with spaceship_tail picture
+    """
+    points = [
+        (spaceship.get_width() / 2, spaceship.get_height() * (3 / 4) + 4),
+        (spaceship.get_width() * (3 / 4) - 4, spaceship.get_height() - 1),
+        (spaceship.get_width() / 4 + 4, spaceship.get_height() - 1),
+        (spaceship.get_width() / 2, spaceship.get_height() * (3 / 4) + 4)
+    ]
+    draw.aalines(spaceship, color, True, points)
+
+    return spaceship
+
+
+def collides_with(game_object, other_game_object) -> bool:
+    """
+    Checking that two objects collided
+
+    :param game_object: GameObject to check the collision
+    :param other_game_object: Other GameObject to check the collision
+    :return: Is objects collide
+    """
+    distance = game_object.position.distance_to(other_game_object.position)
+    return distance < game_object.radius + other_game_object.radius
+
+
+def get_random_position_object_on_screen(screen_width, screen_height, object_width, object_height):
+    return Vector2(random.randint(object_width, screen_width - object_width),
+                   random.randint(object_height, screen_height - object_height))
+

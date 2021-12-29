67510078ae324f9d8111088e5932ad113a9c6253
igor <ighor.naumov.2002mail@mail.ru>
2021-12-29 16:52:25 +0500

    max speed = 0

--- modules/spacehip_health.py
+++ modules/spacehip_health.py
@@ -1,4 +1,5 @@
 from pygame.math import Vector2
+from pygame.surface import Surface
 
 from modules.game_object import GameObject
 
@@ -8,4 +9,7 @@ class SpaceshipHealth(GameObject):
     Game object spaceship health
     """
     def __init__(self, position, sprite):
-        super().__init__(position, sprite, Vector2(0))
+        super().__init__(position, sprite, Vector2(0), 0, 0)
+
+    def move(self, surface: Surface) -> None:
+        pass

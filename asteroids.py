9c892f575ef48f1fbd9fd774f43c414a4aa0cc23
igor <ighor.naumov.2002mail@mail.ru>
2021-12-29 16:51:24 +0500

    add BlackHole and WhiteHole

--- asteroids.py
+++ asteroids.py
@@ -1,15 +1,21 @@
 import sys
 import random
+import time
 from pathlib import Path
 from typing import List
+from random import randint
 
 import pygame
 
 from modules.spaceship import Spaceship
 from modules.asteroid import Asteroid
 from modules.spacehip_health import SpaceshipHealth
-from modules.utils import create_spaceship_picture
+from modules.utils import create_spaceship_picture, get_random_position_object_on_screen
 from modules.colors import BLACK, WHITE
+from modules.blackhole import BlackHole
+from modules.whitehole import WhiteHole
+from modules.bullet import Bullet
+from game_params_one import *
 
 
 class Asteroids:
@@ -17,9 +23,11 @@ class Asteroids:
     Сlass that initializes the game and provides the start of the game cycle
     """
 
-    def __init__(self, width: int = 1600, height: int = 1200, asteroid_width: int = 80, asteroid_height: int = 80,
-                 asteroid_count: int = 4, spaceship_width: int = 32, spaceship_height: int = 32,
-                 spaceship_health_point: int = 4, bullet_width: int = 4, bullet_height: int = 4):
+    def __init__(self, width: int, height: int, asteroid_width: int, asteroid_height: int,
+                 asteroid_count: int, spaceship_width: int, spaceship_height: int,
+                 spaceship_health_point: int, bullet_width: int, bullet_height: int,
+                 black_hole_width: int, black_hole_height: int, black_hole_mass: float,
+                 white_hole_width: int, white_hole_height: int, white_hole_mass: float):
         """
         :param width: Screen width
         :param height: Screen height
@@ -33,6 +41,7 @@ class Asteroids:
         :param bullet_height: Bullet height
         """
         self._init_game()
+        self.clock = pygame.time.Clock()
         self._score_for_asteroids = {(asteroid_width, asteroid_height): 50,
                                      (asteroid_width / 2, asteroid_height / 2): 100,
                                      (asteroid_width / 4, asteroid_height / 4): 200
@@ -40,7 +49,6 @@ class Asteroids:
         self.screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
         self.position_asteroids = ((0, 0), (self.screen.get_width(), 0), (0, self.screen.get_height()),
                                    (self.screen.get_width(), self.screen.get_height()))
-        self.clock = pygame.time.Clock()
         self.asteroid_max_width = asteroid_width
         self.asteroid_max_height = asteroid_height
         self.asteroid_count = asteroid_count
@@ -50,8 +58,20 @@ class Asteroids:
         self.bullets = []
         self.spaceship = Spaceship((self.screen.get_width() / 2, self.screen.get_height() / 2), spaceship_width,
                                    spaceship_height,
-                                   bullet_width, bullet_height, spaceship_health_point, self.bullets.append)
+                                   bullet_width, bullet_height, spaceship_health_point, 8, 8, self.bullets.append)
         self.spaceship_healths = self._create_spaceship_health(spaceship_health_point)
+        self.black_hole = None
+        self.black_hole_width = black_hole_width
+        self.black_hole_height = black_hole_height
+        self.black_hole_mass = black_hole_mass
+        self.time_to_spawn_black_hole = time.perf_counter() + randint(15, 30)
+        self.time_to_delete_black_hole = time.perf_counter()
+        self.white_hole = None
+        self.white_hole_width = white_hole_width
+        self.white_hole_height = white_hole_height
+        self.white_hole_mass = white_hole_mass
+        self.time_to_spawn_white_hole = time.perf_counter() + randint(15, 30)
+        self.time_to_delete_white_hole = time.perf_counter()
         self.font = pygame.font.Font(Path(Path(__file__).parent, 'assets', 'Hyperspace.otf'), 30)
         self._score = 0
 
@@ -65,6 +85,53 @@ class Asteroids:
             self._handle_input()
             self._process_game_logic()
             self._draw()
+
+    def _white_hole_spawn(self):
+        is_white_hole_spawn = self._hole_spawn(self.white_hole,
+                                               self.time_to_spawn_white_hole,
+                                               self.white_hole_width,
+                                               self.white_hole_height,
+                                               self.white_hole_mass,
+                                               WhiteHole)
+        if is_white_hole_spawn:
+            self.white_hole, self.time_to_delete_white_hole = is_white_hole_spawn
+
+    def _white_hole_delete(self):
+        is_white_hole_delete = self._hole_delete(self.white_hole,
+                                                 self.time_to_delete_white_hole)
+        if is_white_hole_delete:
+            self.white_hole, self.time_to_spawn_white_hole = is_white_hole_delete
+
+    def _black_hole_spawn(self):
+        is_black_hole_spawn = self._hole_spawn(self.black_hole,
+                                               self.time_to_spawn_black_hole,
+                                               self.black_hole_width,
+                                               self.black_hole_height,
+                                               self.black_hole_mass,
+                                               BlackHole)
+        if is_black_hole_spawn:
+            self.black_hole, self.time_to_delete_black_hole = is_black_hole_spawn
+
+    def _black_hole_delete(self):
+        is_black_hole_delete = self._hole_delete(self.black_hole,
+                                                 self.time_to_delete_black_hole)
+        if is_black_hole_delete:
+            self.black_hole, self.time_to_spawn_black_hole = is_black_hole_delete
+
+    def _hole_spawn(self, hole, time_to_spawn, hole_width, hole_height, hole_mass, hole_cls):
+        if not hole:
+            if time.perf_counter() > time_to_spawn:
+                hole = hole_cls(get_random_position_object_on_screen(
+                    self.screen.get_width(), self.screen.get_height(), hole_width, hole_height),
+                    pygame.Surface((hole_width, hole_height), pygame.SRCALPHA, 32),
+                    hole_mass
+                )
+                return hole, time.perf_counter() + randint(10, 15)
+
+    def _hole_delete(self, hole, time_to_delete):
+        if hole:
+            if time.perf_counter() > time_to_delete:
+                return None, time.perf_counter() + randint(15, 30)
 
     def _create_spaceship_health(self, spaceship_health_point: int) -> List[SpaceshipHealth]:
         spaceship_healths = []
@@ -106,27 +173,39 @@ class Asteroids:
 
             if is_key_pressed[pygame.K_w]:
                 self.spaceship.accelerate()
+                self.spaceship.tail.activate_tail()
+            else:
+                self.spaceship.tail.deactivate_tail()
 
-    def _asteroid_collides_with_spaceship(self):
-        if self.spaceship:
+    def _asteroid_collides_with_(self, game_object):
+        if type(game_object) is not Asteroid:
             for asteroid in self.asteroids:
-                if asteroid.collides_with(self.spaceship):
-                    if self.spaceship.health > 0:
-                        self.spaceship = Spaceship((self.screen.get_width() / 2, self.screen.get_height() / 2),
-                                                   self.spaceship.sprite.get_width(),
-                                                   self.spaceship.sprite.get_height(),
-                                                   self.spaceship.bullet_width, self.spaceship.bullet_height,
-                                                   self.spaceship.health - 1,
-                                                   self.bullets.append)
-                        for a in self._get_new_asteroids(asteroid):
-                            self.asteroids.append(a)
-                        self._destroy_asteroid(asteroid)
-                        self._score += self._score_for_asteroids[(asteroid.sprite.get_width(),
-                                                                  asteroid.sprite.get_height())]
-                        self.spaceship_healths.pop()
-                    else:
-                        self.spaceship = None
-                        break
+                if asteroid.collides_with(game_object):
+                    if type(game_object) is Spaceship:
+                        if self.spaceship.health > 0:
+                            self.spaceship = self._get_new_spaceship()
+                            self.spaceship_healths.pop()
+                        else:
+                            self.spaceship = None
+                            break
+
+                    if type(game_object) is Bullet:
+                        self.bullets.remove(game_object)
+
+                    for a in self._get_new_asteroids(asteroid):
+                        self.asteroids.append(a)
+                    self._destroy_asteroid(asteroid)
+                    self._score += self._score_for_asteroids[(asteroid.sprite.get_width(),
+                                                              asteroid.sprite.get_height())]
+                    break
+
+    def _get_new_spaceship(self):
+        return Spaceship((self.screen.get_width() / 2, self.screen.get_height() / 2),
+                         self.spaceship.sprite.get_width(),
+                         self.spaceship.sprite.get_height(),
+                         self.spaceship.bullet_width, self.spaceship.bullet_height,
+                         self.spaceship.health - 1, 8, 8,
+                         self.bullets.append)
 
     def _destroy_asteroid(self, asteroid):
         self.asteroids.remove(asteroid)
@@ -139,17 +218,18 @@ class Asteroids:
                                asteroid.sprite.get_height() / 2) for _ in range(2)]:
                 yield a
 
-    def _asteroids_collides_with_bullet(self):
-        for bullet in self.bullets:
-            for asteroid in self.asteroids:
-                if asteroid.collides_with(bullet):
-                    for a in self._get_new_asteroids(asteroid):
-                        self.asteroids.append(a)
-                    self._destroy_asteroid(asteroid)
-                    self._score += self._score_for_asteroids[(asteroid.sprite.get_width(),
-                                                              asteroid.sprite.get_height())]
-                    self.bullets.remove(bullet)
-                    break
+    def _blackhole_collides_with(self, black_hole, game_object):
+        if black_hole.collides_with(game_object):
+            if type(game_object) is Asteroid:
+                self._destroy_asteroid(game_object)
+            if type(game_object) is Bullet:
+                self.bullets.remove(game_object)
+            if type(game_object) is Spaceship:
+                if self.spaceship.health > 0:
+                    self.spaceship = self._get_new_spaceship()
+                    self.spaceship_healths.pop()
+                else:
+                    self.spaceship = None
 
     def _when_all_asteroids_destroyed(self):
         if len(self.asteroids) == 0:
@@ -167,11 +247,21 @@ class Asteroids:
     def _process_game_logic(self):
         for game_object in self._get_game_objects():
             game_object.move(self.screen)
+            if self.black_hole:
+                self.black_hole.pull(game_object)
+                self._blackhole_collides_with(self.black_hole, game_object)
 
-        self._asteroids_collides_with_bullet()
+            if self.white_hole:
+                self.white_hole.repulsion(game_object)
+
+            self._asteroid_collides_with_(game_object)
+
         self._when_all_asteroids_destroyed()
-        self._asteroid_collides_with_spaceship()
         self._delete_bullets_outside_screen()
+        self._black_hole_spawn()
+        self._black_hole_delete()
+        self._white_hole_spawn()
+        self._white_hole_delete()
         if not self.spaceship:
             pygame.quit()
             sys.exit()
@@ -188,10 +278,19 @@ class Asteroids:
         for spaceship_health in self.spaceship_healths:
             spaceship_health.draw(self.screen)
         self._draw_score(self.screen)
+        if self.black_hole:
+            self.black_hole.draw(self.screen)
+        if self.white_hole:
+            self.white_hole.draw(self.screen)
         pygame.display.flip()
         self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    asteroids = Asteroids()
+    asteroids = Asteroids(SCREEN_WIDTH, SCREEN_HEIGHT,
+                          ASTEROID_WIDTH, ASTEROID_HEIGHT, ASTEROID_COUNT,
+                          SPACESHIP_WIDTH, SPACESHIP_HEIGHT, SPACESHIP_HEALTH_POINT,
+                          BULLET_WIDTH, BULLET_HEIGHT,
+                          BLACK_HOLE_WIDTH, BLACK_HOLE_HEIGHT, BLACK_HOLE_MASS,
+                          WHITE_HOLE_WIDTH, WHITE_HOLE_HEIGHT, WHITE_HOLE_MASS)
     asteroids.main_loop()
